6b2095cecc17335c1c624cd14fb7d18251e2da1c
aviescobar <aviescobar1710@gmail.com>
2024-11-21 15:00:00 -0600

    ejersicios

---
+++ adivina_numero.py
@@ -0,0 +1,21 @@
+import random
+
+
+def adivina_el_numero():
+    numero_secreto = random.randint(1, 100)
+    intentos = 0
+
+
+    while True:
+        intento = int(input("Adivina un número entre 1 y 100: "))
+        intentos += 1
+
+        if intento < numero_secreto:
+            print("El número es mayor.")
+        elif intento > numero_secreto:
+            print("El número es menor.")
+        else:
+            print(f"¡Felicidades! Adivinaste el número en {intentos} intentos.")
+            break
+
+adivina_el_numero()
