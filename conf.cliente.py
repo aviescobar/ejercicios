1377ee7dc04acca341ea20efa877a39e91454847
aviescobar <aviescobar1710@gmail.com>
2025-03-30 12:00:00 -0600

    Commit automático de conf.cliente.py en 2025-03-30T12:00:00

--- conf.cliente.py
+++ conf.cliente.py
@@ -31,3 +31,4 @@ def enviar_mensajes():
 # Iniciar hilos para enviar y recibir mensajes simultáneamente
 threading.Thread(target=recibir_mensajes).start()
 threading.Thread(target=enviar_mensajes).start()# Commit automático del día 9
+# Commit automático del día 9
