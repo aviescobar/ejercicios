a7739da8827c3b6b8f0dd75605e27de8314ed117
aviescobar <aviescobar1710@gmail.com>
2025-03-31 12:00:00 -0600

    Commit automático de confi-cliente.py en 2025-03-31T12:00:00

--- confi-cliente.py
+++ confi-cliente.py
@@ -20,3 +20,4 @@ print("Señal de apagado enviada al cliente.")
 cliente_socket.close()
 servidor.close()# Commit automático del día 10
 # Commit automático del día 10
+# Commit automático del día 10
